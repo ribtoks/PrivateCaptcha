86a6b9e349de28ea9dbc4843c11d5fc72f2234d4
Taras Kushnir <tk.dev@mailbox.org>
2025-06-17 14:54:11 +0300

    Update ClickHouse image also in tests
    change from f031d0b335f869541679588c462f759ce7d253d7

--- docker/run-clickhouse.sh
+++ docker/run-clickhouse.sh
@@ -12,7 +12,7 @@ docker run -d --rm \
     -v $(pwd)/docker/clickhouse-config.xml:/etc/clickhouse-server/config.d/myconfig.xml \
     -v $(pwd)/docker/clickhouse-users.xml:/etc/clickhouse-server/users.d/myusers.xml \
     -v $(pwd)/pkg/db/migrations/init/clickhouse.sql:/docker-entrypoint-initdb.d/init.sql \
-    clickhouse/clickhouse-server:24.8.7-alpine
+    clickhouse/clickhouse-server:24.12.6-alpine
 
 echo "Waiting for clickhouse healthcheck..."
 
