207d8a0f6888e506464019a3ace861fd3e2097cf
Taras Kushnir <tk.dev@mailbox.org>
2025-12-19 14:44:24 +0100

    Fix api key scope migration

--- pkg/db/migrations/postgres/000112_add_apikey_scope_column.up.sql
+++ pkg/db/migrations/postgres/000112_add_apikey_scope_column.up.sql
@@ -1,5 +1,7 @@
 CREATE TYPE backend.apikey_scope AS ENUM ('puzzle', 'portal');
 
-ALTER TABLE backend.apikeys ADD COLUMN scope backend.apikey_scope NOT NULL;
+ALTER TABLE backend.apikeys ADD COLUMN scope backend.apikey_scope;
 
 UPDATE backend.apikeys SET scope = 'puzzle'::backend.apikey_scope;
+
+ALTER TABLE backend.apikeys ALTER COLUMN scope SET NOT NULL;
